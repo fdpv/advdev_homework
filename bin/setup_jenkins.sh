9130558ad572232a7090761f7ef96f21bc5aeb57
Daniel Parra <fdparra@gfi.es>
2019-10-10 19:44:14 +0200

    Builds the maven+skopeo image for the Jenkins Agent

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -20,6 +20,9 @@ oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=
 
 # Create custom agent container image with skopeo
 # TBD
+oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
+       USER root\nRUN yum -y install skopeo && yum clean all\n
+       USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # TBD
